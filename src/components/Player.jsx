8a05d6a2ecfe9e38a775c35bbb4a6ef1989826c1
Katiasun <katjatolok3@gmail.com>
2025-02-20 14:52:32 +0100

    create function handlcehange in file player.jsx

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,17 +3,21 @@ import { useState } from "react";
 export default function Player({ initalName, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
   const [playerName, setPlayerName] = useState(initalName);
+
   function handleEditClick() {
     setIsEditing((editing) => !editing);
   }
 
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
   let editablePlayerName = <span className="player-name">{playerName}</span>;
-  // const btnCaption = "Edit";
 
   if (isEditing) {
-    editablePlayerName = <input type="text" required value={playerName} />;
-    // btnCaption = "Save";
+    editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
   }
+
   return (
     <li>
       <span className="player">
