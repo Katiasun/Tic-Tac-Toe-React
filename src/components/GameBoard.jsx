fbb4a84d5a34b2db93e33d4861ccc6e0f3ad66af
Katiasun <katjatolok3@gmail.com>
2025-02-20 16:06:10 +0100

    write code inside component GameBoard

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -4,4 +4,20 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard() {}
+export default function GameBoard() {
+  return (
+    <ol id="game-board">
+      {initialGameBoard.map((row, rowIndex) => {
+        <li key={rowIndex}>
+          <ol>
+            {row.map((col, colIndex) => {
+              <li key={colIndex}>
+                <button>{col}</button>
+              </li>;
+            })}
+          </ol>
+        </li>;
+      })}
+    </ol>
+  );
+}
