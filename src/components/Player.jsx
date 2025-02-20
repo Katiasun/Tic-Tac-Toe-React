08acc53bd62b6864d3abe487f6418fb95b1494c1
Katiasun <katjatolok3@gmail.com>
2025-02-20 10:26:20 +0100

    change const to let. hange

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,15 +2,16 @@ import { useState } from "react";
 
 export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
+
   function handleEditClick() {
-    setIsEditing(!isEditing);
+    setIsEditing((editing) => !editing);
   }
 
-  const playerName = <span className="player-name">{name}</span>;
+  let playerName = <span className="player-name">{name}</span>;
   // const btnCaption = "Edit";
 
   if (isEditing) {
-    playerName = <input type="text" required />;
+    playerName = <input type="text" required value={name} />;
     // btnCaption = "Save";
   }
   return (
@@ -19,7 +20,7 @@ export default function Player({ name, symbol }) {
         {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>{btnCaption ? "Save" : "Edit"}</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
