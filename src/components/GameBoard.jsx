ac58bd9bf4613be440e61c6b7ab777d6da6d3f8c
Katiasun <katjatolok3@gmail.com>
2025-02-23 15:16:41 +0100

    removed dead code, made the code a little more streamlined

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,31 +1,4 @@
-// const initialGameBoard = [
-//   [null, null, null],
-//   [null, null, null],
-//   [null, null, null],
-// ];
-
 export default function GameBoard({ onSelectSquare, board }) {
-  // let gameBoard = initialGameBoard;
-
-  // for (const turn of turns) {
-  //   const { square, player } = turn;
-  //   const { row, col } = square;
-
-  //   gameBoard[row][col] = player;
-  // }
-
-  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  // function handleSelectSquare(rowIndex, colIndex) {
-  //   setGameBoard((prevGameBoard) => {
-  //     const updateBoard = [...prevGameBoard.map((innerArray) => [...innerArray])];
-  //     updateBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //     return updateBoard;
-  //   });
-
-  //   onSelectSquare();
-  // }
-
   return (
     <ol id="game-board">
       {board.map((row, rowIndex) => (
